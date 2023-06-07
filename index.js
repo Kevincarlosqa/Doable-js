a84d371f5334b06f583f23efce9533d702ad4f9b
Kevin Quispe <kevincarlosqa@gmail.com>
2023-06-07 18:10:27 -0500

    add: Login Service

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import DOMHandler from "./scripts/dom-handler.js";
 import loginPage from "./scripts/pages/login-page.js";
+import { login } from "./scripts/services/session-service.js";
 
-const root = document.querySelector("#root")
-let load
-DOMHandler.load(loginPage(), root)
+// const root = document.querySelector("#root")
+// let load
+// DOMHandler.load(loginPage(), root)
+
+const credentials = {
+	email: "kevincarlosqa@mail.com",
+	password: "123456"
+}
+
+login(credentials).then(user => console.log(user))
